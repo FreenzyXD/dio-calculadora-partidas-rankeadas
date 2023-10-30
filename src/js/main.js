6ec904a4bdd058337d83e88c7a3ac467480f0ddb
Freenzy <nicolasjanes69@gmail.com>
2023-10-29 23:17:26 -0300

    inicio de desenvolvimento

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+let vitórias = parseInt()
+let derrotas = parseInt()
+function calculaRank(){
+
+
+}
+function descobreRank(){
+
+        switch  (true) {
+            case qtdVitorias < 10:
+                mensagem = "voce é ferro";
+                break
+            case qtdVitorias >10 && qtdVitorias <=20:
+                mensagem = "voce é bronze"  
+
+
+
+        }
+
+
+
+}
